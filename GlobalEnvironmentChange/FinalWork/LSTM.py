baa20b165409afdab5ca9bb16ec3a32829eb784c
AEight1973 <1015252562@163.com>
2021-04-06 14:31:53 +0800

    Default ChangeList

--- GlobalEnvironmentChange/FinalWork/LSTM.py
+++ GlobalEnvironmentChange/FinalWork/LSTM.py
@@ -1,6 +1,48 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import keras
+from keras.datasets import imdb
 
-model = keras.models.Sequential()
-model.add()
+# keras实现LSTM网络
+# Hyper parameters
+from keras.utils import np_utils
+from tensorflow.python.keras import Sequential
+
+batch_size = 128
+nb_epoch = 10
+nb_time_steps = 12
+dim_input_vector = 1
+nb_classes = 10
+
+
+# 载入数据lmdb
+(X_train, Y_train), (X_test, Y_test) = imdb.load_data()
+
+X_train = X_train.reshape(-1, 28, 28) / 255
+X_test = X_test.reshape(-1, 28, 28) / 255
+
+Y_train = np_utils.to_categorical(Y_train, num_classes=10)
+Y_test = np_utils.to_categorical(Y_test, num_classes=10)
+
+input_shape = (nb_time_steps, dim_input_vector)
+# nb_time_steps时间步 ， dim_input_vector 表示输入向量的维度，也等于n_features，列数
+X_train = X_train.astype('float32') / 255.
+X_test = X_test.astype('float32') / 255.
+
+Y_train = np_utils.to_categorical(y_train, nb_classes)  # 转为类别向量，nb_classes为类别数目
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+# Build LSTM network
+model = Sequential()
+model.add(LSTM(nb_lstm_outputs, input_shape=input_shape))
+model.add(Dense(nb_classes, activation='softmax', init=init_weights))  # 初始权重
+
+model.summary()  # 打印模型
+plot(model, to_file='lstm_model.png')  # 绘制模型结构图，并保存成图片
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])  # 编译模型
+history = model.fit(X_train, Y_train, nb_epoch=nb_epoch, batch_size=batch_size, shuffle=True, verbose=1)  # 迭代训练
+
+score = model.evaluate(X_test, Y_test, verbose=1)  # 模型评估
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
---
+++ GlobalEnvironmentChange/FinalWork/LoadData.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import metpy.calc as mpcalc
+from metpy.plots import Hodograph, SkewT
+from metpy.units import units
+import metpy
+import numpy as np
+
+
+def load_data(index):
+    col_names = ['pressure', 'height', 'temperature', 'dewpoint', 'direction', 'speed']
+    df = pd.read_csv("58362_{}.csv", encoding='gbk'.format(index))
+    df = df[col_names]
+    df = df.dropna(axis=0, how='any')
+
+    return df
+
+
+def df2datasets(df):
+    x = np.array(df['pressure', 'height', 'dewpoint', 'direction', 'speed'])
+    y = np.array(df['temperature'])
+
+    return x, y
+
+
+def paint(df):
+    p = df['pressure'].values * units.hPa  # 单位：hPa
+    t = df['temperature'].values * units.degC  # 单位：℃
+    td = df['dewpoint'].values * units.degC  # 单位：℃
+    wind_speed = df['speed'].values * units.knots  # 单位：knot
+    wind_dir = df['direction'].values * units.degrees  # 单位：°
+    u, v = mpcalc.wind_components(wind_speed, wind_dir)  # 计算水平风速u和v
+    prof = mpcalc.parcel_profile(p, t[0], td[0]).to('degC')
+
+    fig = plt.figure(figsize=(9, 9))
+    skew = SkewT(fig)
+
+    skew.plot(p, t, 'r', linewidth=2)  # 绘制层结曲线（红色）
+    skew.plot(p, td, 'g', linewidth=2)  # 绘制露点曲线（绿色）
+    skew.plot_barbs(p, u, v)  # 绘制风羽
+    skew.plot(p, prof, 'k')
+
+    skew.ax.set_ylim(1000, 200)
+    skew.ax.set_xlim(-20, 60)
+
+    # 显示图像
+    plt.show()

45685e7335f3b492047b58639df006201887ebcf
AEight1973 <1015252562@163.com>
2021-04-20 15:21:41 +0800

    Default ChangeList

--- GlobalEnvironmentChange/FinalWork/DownloadData.py
+++ GlobalEnvironmentChange/FinalWork/DownloadData.py
@@ -2,6 +2,7 @@ import datetime
 from siphon.simplewebservice.wyoming import WyomingUpperAir
 import os
 import pandas as pd
+from tqdm import tqdm
 
 start = datetime.datetime(2008, 1, 1, 0)
 end = datetime.datetime(2020, 12, 31, 12)
@@ -17,13 +18,14 @@ stationlist = list(stationfile['区站号'])
 
 # 设置下载区间 (数据集共计817个站点数据 其中中国站点为 [164: 252])
 # 设置区间起始点 单点下载时间较长 每次下载20个站点
-station_range_start = 100
+station_range_start = 80
 station_range_step = 20
 
 # 批量下载
 for station in stationlist[station_range_start: station_range_start + station_range_step]:
     station = str(station)
-    for date in datelist:
+    print('正在下载station:'+station)
+    for date in tqdm(datelist):
         try:
             # 新建文件夹
             dirs = 'data/' + station

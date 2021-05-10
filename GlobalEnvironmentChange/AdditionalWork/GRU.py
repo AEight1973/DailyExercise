3b866e1ccad17b4bc095904bc52221d8e0d195ed
AEight1973 <1015252562@163.com>
2021-05-11 00:03:13 +0800

    Change from Desktop_T70RDMU

--- GlobalEnvironmentChange/AdditionalWork/GRU.py
+++ GlobalEnvironmentChange/AdditionalWork/GRU.py
@@ -1,3 +1,5 @@
+import os
+from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import TensorDataset, DataLoader
 import torch
@@ -15,8 +17,8 @@ I   具体指标：
     1)  日期: yyyy-mm-dd 星期X
     2)  最高气温: TT℃
     3)  最低气温: tt℃
-    4)  天气: w1(~w2)
-    5)  风向: d1(~d2) n1(-n2)[~n3(-n4)]
+    4)  天气: w
+    5)  风向: d风 n级
 II  中文转编码
     1)  天气： {'晴': 0, '多云': 1, '阴': 2,
                '小雨': 3, '阵雨': 4, '中雨': 5,
@@ -32,8 +34,8 @@ with open('data/崇明历史气候记录.txt', 'r+', encoding='utf8') as f:
 
 # 中文转码字典
 weather2dict = {'晴': 0, '多云': 1, '阴': 2,
-               '小雨': 3, '阵雨': 4, '中雨': 5,
-               '大雨': 6, '雷阵雨': 7, '暴雨': 8}
+                '小雨': 3, '阵雨': 4, '中雨': 5,
+                '大雨': 6, '雷阵雨': 7, '暴雨': 8}
 wind2dict = {'北风': 0, '东北风': 1, '东风': 2, '东南风': 3,
              '南风': 4, '西南风': 5, '西风': 6, '西北风': 7}
 
@@ -93,6 +95,8 @@ dataset = pd.DataFrame(data, columns=_data[0, 1:].reshape(-1), index=time)
 batch_size = 128
 n_feature = data.shape[1]
 time_step = 7
+epoch = 50
+pred_num = 20
 
 train_set, test_set = train_test_split(data, train_size=0.8, shuffle=False)
 
@@ -126,106 +130,116 @@ train_dl = DataLoader(dataset=train_ds, batch_size=batch_size, shuffle=True)
 valid_ds = TensorDataset(x_test, y_test)
 valid_dl = DataLoader(dataset=valid_ds, batch_size=batch_size)
 
-# '''model'''
-#
-# class GRU(nn.Module):
-#     def __init__(self):
-#         super(GRU, self).__init__()
-#         self.gru = nn.GRU(input_size=1, hidden_size=10, num_layers=2, dropout=0.2, batch_first=True)
-#         self.linear = nn.Linear(in_features=10, out_features=1)
-#
-#     def forward(self, _x):
-#         _x, _ = self.gru(_x)
-#         s, b, h = _x.shape
-#         _x = _x.contiguous().view(s * b, h)  # 转换成线性层的输入格式
-#         _x = self.linear(_x)
-#         _x = _x.view(s, b, -1)
-#         return _x
-#
-# if cuda_or_not:
-#     model = GRU().cuda()
-# else:
-#     model = GRU()
-#
-# criterion = nn.MSELoss()
-# optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
-#
-# # 开始训练
-# Loss, Valid_Loss = [], []
-# for e in range(epoch):
-#     model.train()
-#
-#     loss = 0
-#     for xb, yb in train_dl:
-#         # 转化数据
-#         if cuda_or_not:
-#             var_x = Variable(xb).cuda()
-#             var_y = Variable(xb).cuda()
-#         else:
-#             var_x = Variable(xb)
-#             var_y = Variable(xb)
-#
-#         # 前向传播
-#         out = model(var_x)
-#         _loss = criterion(out, var_y)
-#
-#         # 反向传播
-#         optimizer.zero_grad()
-#         _loss.backward()
-#         optimizer.step()
-#
-#         loss += _loss.item()
-#
-#     # 加入验证集
-#     model.eval()  # 评估模型
-#     with torch.no_grad():
-#         if cuda_or_not:
-#             valid_loss = sum(criterion(model(xb.cuda()), yb.cuda()) for xb, yb in valid_dl)
-#         else:
-#             valid_loss = sum(criterion(model(xb), yb) for xb, yb in valid_dl)
-#
-#     print('Epoch: {}, Loss: {:.5f}, Valid_Loss: {:.5f}'.format(e + 1, loss / len(train_dl),
-#                                                                valid_loss / len(valid_dl)))
-#     Loss.append(loss / len(train_dl))
-#     Valid_Loss.append(valid_loss / len(valid_dl))
-#
-# '''save'''
-#
-# _path = 'cache/data/' + _station + '/model'
-# if not os.path.exists(_path):
-#     os.makedirs(_path)
-# PATH = _path + '/interpolation_' + str(c)
-# torch.save(model, PATH)
-#
-# '''predict'''
-# model = model.eval()  # 转换成测试模式
-#
-# start = datetime.datetime(2008, 1, 1, 0)
-# for i in range(time_step, len(_time)):
-#     if _time[i - time_step - 1] == _time[i] - datetime.timedelta(hours=12) * (time_step - 1):
-#         start = _time[i]
-#         break
-# end = datetime.datetime(2019, 12, 31, 12)
-# datelist = []
-# while start <= end:
-#     datelist.append(start)
-#     start += datetime.timedelta(hours=12)
-#
-# predict_time = []
-# predict_data = []
-# for t in datelist:
-#     if t in _time:
-#         continue
-#     else:
-#         _data = []
-#         for i in range(time_step, 0, -1):
-#             _t = t - datetime.timedelta(hours=12) * i
-#             if _t in _time:
-#                 _data.append(list(_dataset.loc[_t]))
-#             else:
-#                 _data.append(predict_data[predict_time.index(_t)])
-#         var_predict = Variable(torch.FloatTensor(_data).view(-1, time_step, n_feature)).cuda()
-#         pred_predict = model(var_predict)
-#         predict_data.append(list(pred_predict.data.cpu().numpy()[0, -1, :]))
-#         predict_time.append(_time)
-# return pd.DataFrame(predict_data, index=predict_time, columns=_dataset.columns)
+'''model'''
+class GRU(nn.Module):
+    def __init__(self):
+        super(GRU, self).__init__()
+        self.gru = nn.GRU(input_size=1, hidden_size=10, num_layers=2, dropout=0.2, batch_first=True)
+        self.linear = nn.Linear(in_features=10, out_features=1)
+
+    def forward(self, _x):
+        _x, _ = self.gru(_x)
+        s, b, h = _x.shape
+        _x = _x.contiguous().view(s * b, h)  # 转换成线性层的输入格式
+        _x = self.linear(_x)
+        _x = _x.view(s, b, -1)
+        return _x
+
+
+model = GRU().cuda()
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+
+# 开始训练
+Loss, Valid_Loss = [], []
+for e in range(epoch):
+    model.train()
+
+    loss = 0
+    for xb, yb in train_dl:
+        # 转化数据
+        var_x = Variable(xb).cuda()
+        var_y = Variable(xb).cuda()
+
+        # 前向传播
+        out = model(var_x)
+        _loss = criterion(out, var_y)
+
+        # 反向传播
+        optimizer.zero_grad()
+        _loss.backward()
+        optimizer.step()
+
+        loss += _loss.item()
+
+    # 加入验证集
+    model.eval()  # 评估模型
+    with torch.no_grad():
+        valid_loss = sum(criterion(model(xb.cuda()), yb.cuda()) for xb, yb in valid_dl)
+
+    print('Epoch: {}, Loss: {:.5f}, Valid_Loss: {:.5f}'.format(e + 1, loss / len(train_dl),
+                                                               valid_loss / len(valid_dl)))
+    Loss.append(loss / len(train_dl))
+    Valid_Loss.append(valid_loss / len(valid_dl))
+
+'''save'''
+
+path = 'model'
+if not os.path.exists(path):
+    os.makedirs(path)
+PATH = path + '/model_' + datetime.datetime.now().strftime('%m-%d_%H:%m')
+torch.save(model, PATH)
+
+'''predict'''
+# 测试集输入模型进行预测
+model = model.eval()  # 转换成测试模式
+
+# 对真实数据进行绘图
+real_data = dataset.values
+real_time = dataset.index
+plt.plot(real_time, real_data, color='red', label='40M Real Temperature')
+
+# 对训练数据进行绘图
+var_train = Variable(x_train).cuda()
+pred_train = model(var_train)
+# 改变输出的格式
+pred_train = pred_train.data.cpu().numpy()[:, -1, 0].reshape(-1, 1)
+# 对训练数据还原---从（0，1）反归一化到原始范围
+train_temp = sc.inverse_transform(pred_train)
+# 画出真实数据和预测数据的对比曲线
+train_time = real_time[time_step: len(train_set)]
+plt.plot(train_time, train_temp, color='blue', label='40M Train Temperature')
+
+# 对测试数据进行绘图
+var_test = Variable(x_test).cuda()
+pred_test = model(var_test)
+# 改变输出的格式
+pred_test = pred_test.data.cpu().numpy()[:, -1, 0].reshape(-1, 1)
+# 对测试数据还原---从（0，1）反归一化到原始范围
+test_temp = sc.inverse_transform(pred_test)
+# 画出真实数据和预测数据的对比曲线
+test_time = real_time[len(train_set) + time_step:]
+plt.plot(test_time, test_temp, color='green', label='40M Test Temperature')
+
+# 对未来一年进行预测
+predict_time = []
+predict_data = list(sc.transform(real_data).T[0])
+_time = real_time[-1]
+for i in range(predict_data):
+    if _time.day == 30:
+        _time = datetime.date(_time.year + 1, 6, 1)
+    else:
+        _time += datetime.timedelta(days=1)
+    var_predict = Variable(torch.FloatTensor(predict_data[-time_step:]).view(-1, time_step, n_feature)).cuda()
+    pred_predict = model(var_predict)
+    predict_data.append(pred_predict.data.cpu().numpy()[0, -1, 0])
+    predict_time.append(_time)
+predict_temp = sc.inverse_transform(np.array(predict_data[-pred_num:]).reshape(-1, 1))
+plt.plot(predict_time, predict_temp, color='orange', label='40M Predict Temperature')
+
+
+plt.title('40M Temperature Training Consult')
+plt.xlabel('Time')
+plt.ylabel('Temperature')
+plt.legend()
+plt.show()
